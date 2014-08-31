23e8dd6bc720afe9670d936c13e49987a46a7a22
Mitul <abc@ABCs-Mac-mini.local>
2014-08-31 14:06:10 +0530

    swift hello world program using ViewConoller (XIB)
    Create Simple viewController Demo Using Swift Programming language
    using XIB
    
    find More Detail at :
    http://iosdevelopercloud.wordpress.com/2014/08/25/swift-hello-world-prog
    ram-using-viewconoller-base-xib/

---
+++ DemoViewController/DemoRootViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DemoRootViewController.swift
+//  DemoViewController
+//
+//  Created by Mitul on 25/08/14.
+//  Copyright (c) 2014 vishva. All rights reserved.
+//
+
+import UIKit
+
+class DemoRootViewController:UIViewController
+{
+    init(nibName nibNameOrNil: String? , bundle nibBundleOrNil:NSBundle?){
+        super.init(nibName:nibNameOrNil , bundle: nibBundleOrNil);
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidLoad(){
+        super.viewDidLoad();
+    }
+
+
+}
